ccf4d9361d97d5cb97d2149e6e55ee7e83c8101e
sandeep224422 <sandeep.5519051923@ipu.ac.in>
2025-09-07 13:28:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ SUPPORT_GRP = getenv("SUPPORT_GRP", "brahix_support")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "about_brahix")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "Brahix")
 GEMINI_API_KEY = getenv("GEMINI_API_KEY", "AIzaSyDFM-OzXtqz7bk9GEd9FfOiC0IAPYvo4h8")
-OPENAI_API_KEY = getenv("OPENAI_API_KEY", "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") 
+OPENAI_API_KEY = getenv("OPENAI_API_KEY", "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") 
 
 # Random Start Images
 IMG = [
